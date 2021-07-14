c9afd7808acb20bb638ddb79437f611f83407153
shubham halade <84590702+shubhhu-git@users.noreply.github.com>
2021-07-14 17:12:36 +0530

    sign up backend

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8000 ;
 const hbs = require("hbs");
-const { connect } = require("http2");
-const path = require("path");
+//const { connect } = require("http2");
+
 require("../src/db/conn");
 const Student = require("../src/models/students");
 const staticPath = path.join(__dirname , "../public");
 const templatePth = path.join(__dirname , "../main/views");
 const partialsPath = path.join(__dirname , "../main/partials");
 const studentRouter = require("../src/routers/student");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
 app.use(studentRouter);
 app.set("views" , templatePth);
 app.set("view engine" , "hbs");
 hbs.registerPartials(partialsPath);
-app.use(express.json());
 app.use(express.static(staticPath));
 
 
@@ -26,37 +32,6 @@ app.get("/login" , (req , res) => {
 });
 app.get("/signup" , (req , res) => {
     res.render("signup");
-});
-
-app.post("/signup" , async(req , res) =>{
-    try{
-        const password = req.body.password;
-        const cpassword = req.body.cpassword;
-
-        if(password === cpassword){
-            const user = new Student({
-                firstname: req.body.fname,
-                lastname: req.body.lname,
-                email: req.body.email,
-                phone: req.body.phone,
-                birthday: req.body.day,
-                birthmonth: req.body.month,
-                birthyear: req.body.year,
-                password: req.body.password,
-                cpassword: req.body.cpassword
-            });
-            console.log("success" + user);
-            const registered = await user.save();
-          //  console.log("token" + token);
-            console.log("page" + registered);
-            res.status(201).render("login.hbs");
-        }else{
-            res.status(401).send("password are not matching");
-        }
-
-    }catch(err){
-        res.status(400).send(err);
-    }
 });
 
 app.get("/explore" , (req , res) => {
--- src/routers/student.js
+++ src/routers/student.js
@@ -3,11 +3,35 @@ const router = new express.Router();
 const Student = require("../models/students");
 
 
+router.post("/signup" , async(req , res) =>{
+    try{
+        const password = req.body.password;
+        const cpassword = req.body.cpassword;
 
+        if(password === cpassword){
+            const user = new Student({
+                fname: req.body.fname,
+                lname: req.body.lname,
+                email: req.body.email,
+                phone: req.body.phone,
+                day: req.body.day,
+                month: req.body.month,
+                year: req.body.year,
+                password: req.body.password,
+                cpassword: req.body.cpassword
+            });
+            console.log("success" + user);
+            const registered = await user.save();
+          //  console.log("token" + token);
+            console.log("page" + registered);
+            res.status(201).render("login.hbs");
+        }else{
+            res.send("password are not matching");
+        }
 
-
-
-
-
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
 
 module.exports = router;
